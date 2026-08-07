24350e12b33bff3cba495044e79024dc0f8498c2
ERROR <131444381+error-wtf@users.noreply.github.com>
2026-08-07 22:37:38 +0200

    Sync certified canonical Weyl status

--- src/hedenmalm/volterra_closure.py
+++ src/hedenmalm/volterra_closure.py
@@ -8,8 +8,8 @@ def spectral_volterra_closure_status() -> dict[str, str]:
         "cosine_channel": "CONTROLLED_BY_XI_ZERO",
         "sine_channel": "NOT_CONTROLLED_BY_XI_ZERO_ALONE",
         "volterra_dynamics": "MUST_BE_USED",
-        "coupled_halfline_energy": "OPEN",
-        "spectral_volterra_closure": "OPEN",
+        "coupled_halfline_energy": "PROVED_CERTIFIED",
+        "spectral_volterra_closure": "PROVED_CERTIFIED",
         "rh_conclusion": "CONDITIONAL_ONLY",
     }
 
